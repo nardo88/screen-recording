c701f685a7b7b10106ea73460c2b3f20ad2e20e7
nardo88 <nardo1988@mail.ru>
2023-09-27 21:52:21 +0300

    node

---
+++ server/index.ts
@@ -0,0 +1,19 @@
+import 'module-alias/register'
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+import { onConnection } from '@socket/onConnection'
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+})
+
+io.on('connection', onConnection)
+
+server.listen(4000, () => {
+  console.log('Server ready 🚀 ')
+})
--- server/socket/onConnection.ts
+++ server/socket/onConnection.ts
@@ -1,42 +1,43 @@
-import { saveData } from "../utils/saveData.js";
+import { saveData } from '@utils/saveData'
+import { Socket } from 'socket.io'
 
-const socketByUser = {};
-const dataChunks = {};
+const socketByUser: Record<string, string> = {}
+const dataChunks: Record<string, any[]> = {}
 
-export const onConnection = (socket) => {
+export const onConnection = (socket: Socket) => {
   // Функция для обработки подключения принимает сокет.
   // Обрабатываем подключение нового пользователя посредством записи имени пользователя в поисковую таблицу.
-  socket.on("user:connected", (username) => {
+  socket.on('user:connected', (username: string) => {
     if (!socketByUser[socket.id]) {
-      socketByUser[socket.id] = username;
+      socketByUser[socket.id] = username
     }
-  });
+  })
 
   // Обрабатываем получение от клиента частей записанных данных. dataChunks — это также поисковая таблица имя пользователя - массив данных.
-  socket.on("screenData:start", ({ data, username }) => {
+  socket.on('screenData:start', ({ data, username }) => {
     if (dataChunks[username]) {
-      dataChunks[username].push(data);
+      dataChunks[username].push(data)
     } else {
-      dataChunks[username] = [data];
+      dataChunks[username] = [data]
     }
-  });
+  })
 
   // Обрабатываем завершение записи.
-  socket.on("screenData:end", (username) => {
+  socket.on('screenData:end', (username) => {
     if (dataChunks[username] && dataChunks[username].length) {
       // вызываем функцию для записи данных,
       // передавая ей массив данных и имя пользователя
-      saveData(dataChunks[username], username);
-      dataChunks[username] = [];
+      saveData(dataChunks[username], username)
+      dataChunks[username] = []
     }
-  });
+  })
 
   // Аналогичным образом обрабатываем отключение клиента на случай закрытия вкладки браузера во время записи — в этом случае событие screenData:end отправлено не будет. В худшем случае мы потеряем 250 мс видео
-  socket.on("disconnect", () => {
-    const username = socketByUser[socket.id];
+  socket.on('disconnect', () => {
+    const username = socketByUser[socket.id]
     if (dataChunks[username] && dataChunks[username].length) {
-      saveData(dataChunks[username], username);
-      dataChunks[username] = [];
+      saveData(dataChunks[username], username)
+      dataChunks[username] = []
     }
-  });
-};
+  })
+}
